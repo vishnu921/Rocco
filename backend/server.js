794952dfd5f2360e13211d444a1de9a60533f02a
vishnu0921 <ba11@iitbbs.ac.in>
2022-08-07 22:26:43 +0530

    user routes and model

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workout')
+const userRoutes = require('./routes/user')
 
 const app = express()
 
@@ -11,6 +12,7 @@ app.use(express.json())
 
 // routes
 app.use('/api/workouts', workoutRoutes)
+app.use('/api/user', userRoutes)
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
