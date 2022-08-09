aa279ffe938864934c1a8795f04800729cfbd3a6
vishnu0921 <ba11@iitbbs.ac.in>
2022-08-09 22:53:21 +0530

    user specific workouts functoinality

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose')
 
 // GET all workouts
 const getWorkouts = async (req, res) => {
-  const workouts = await Workout.find({}).sort({createdAt: -1})
+  const user_id = req.user._id
+
+  const workouts = await Workout.find({ user_id }).sort({createdAt: -1})
 
   res.status(200).json(workouts)
 }
@@ -46,7 +48,8 @@ const createWorkout = async (req, res) => {
   }
 
   try {
-      const workout = await Workout.create({title, reps, sets})
+    const user_id = req.user._id
+      const workout = await Workout.create({title, reps, sets, user_id})
       res.status(200).json(workout)
   } catch (error) {
       res.status(400).json({error: error.message})
--- backend/models/workoutModel.js
+++ backend/models/workoutModel.js
@@ -14,6 +14,10 @@ const workoutSchema = new Schema({
     sets: {
         type: Number,
         required: true
+    },
+    user_id: {
+        type: String,
+        required: true
     }
 }, { timestamps: true })
 
